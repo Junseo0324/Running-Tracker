d0b0896cee729c8d846f44cc3fa11cb9766e36e1
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2026-01-05 16:38:46 +0900

    사용하지 않는 화면(Setup, Statistics) 및 관련 네비게이션 제거
    - `SetupScreen` 및 `StatisticsScreen` 컴포넌트 삭제
    - `Screen` 실드 클래스에서 `SetupScreen`, `StatisticsScreen` 경로 정의 제거
    - `Navigation` 구성 내 불필요한 `composable` 대상 및 임포트 정리

--- app/src/main/java/com/devhjs/runningtracker/presentation/navigation/Navigation.kt
+++ app/src/main/java/com/devhjs/runningtracker/presentation/navigation/Navigation.kt
@@ -6,10 +6,8 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.devhjs.runningtracker.presentation.home.HomeScreen
-import com.devhjs.runningtracker.presentation.home.SetupScreen
 import com.devhjs.runningtracker.presentation.run.ResultScreen
 import com.devhjs.runningtracker.presentation.run.RunScreen
-import com.devhjs.runningtracker.presentation.statistics.StatisticsScreen
 
 @Composable
 fun Navigation(
@@ -22,14 +20,8 @@ fun Navigation(
         composable(Screen.HomeScreen.route) {
             HomeScreen(navController = navController)
         }
-        composable(Screen.SetupScreen.route) {
-            SetupScreen(navController = navController)
-        }
         composable(Screen.RunScreen.route) {
             RunScreen(navController = navController)
-        }
-        composable(Screen.StatisticsScreen.route) {
-            StatisticsScreen(navController = navController)
         }
         composable(Screen.ResultScreen.route) {
             ResultScreen(navController = navController)
--- app/src/main/java/com/devhjs/runningtracker/presentation/navigation/Screen.kt
+++ app/src/main/java/com/devhjs/runningtracker/presentation/navigation/Screen.kt
@@ -3,7 +3,5 @@ package com.devhjs.runningtracker.presentation.navigation
 sealed class Screen(val route: String) {
     object HomeScreen : Screen("home_screen")
     object RunScreen : Screen("run_screen")
-    object StatisticsScreen : Screen("statistics_screen")
-    object SetupScreen : Screen("setup_screen")
     object ResultScreen : Screen("result_screen")
 }
